f038597129768e0b1cbd2c0c3e3ea0f19b13b9ed
ali1536 <92683569+ali1536@users.noreply.github.com>
2022-09-14 10:21:41 +0430

    Add files via upload

--- App.js
+++ App.js
@@ -42,84 +42,82 @@ class MyComponent extends Component {
     render() {
         return (
             <div className='pt-2'>
-                <form>
-                    <h4>Create Order</h4>
-                    <div className="inputsgroup my-2 w-25">
-                        <label htmlFor='username' className="fw-bold form-label">User Name</label>
-                        <input type="text"
-                            value={this.state.username}
-                            onChange={this.handleUsernameChange}
-                            className="border border-dark form-control" required />
-                    </div>
-                    <div className="inputsgroup my-2 w-25">
-                        <label htmlFor='createdat' className="fw-bold form-label">Created at</label>
-                        <input type={'date'}
-                            onChange={this.handleCreatedAtChange}
-                            value={this.state.createdAt}
-                            className="border border-dark form-control" required />
-                    </div>
-                    <div className="inputsgroup my-2 w-25">
-                        <label htmlFor='source' className="fw-bold form-label">Source</label>
-                        <input type="text"
-                            onChange={this.handleSourceChange}
-                            value={this.state.source}
-                            className="border border-dark form-control" required />
-                    </div>
-                    <div className="inputsgroup my-2 w-25">
-                        <label htmlFor='warehouse' className="fw-bold form-label">Warehouse</label>
-                        <input type="text"
-                            onChange={this.handleWarehouseChange}
-                            value={this.state.Warehouse}
-                            className="border border-dark form-control" required />
-                    </div>
-                    <div className="inputsgroup my-2 w-25">
-                        <label htmlFor='ordernumer' className="fw-bold form-label">Order Number</label>
-                        <input type={'number'}
-                            onChange={this.handleOrderNumberChange}
-                            value={this.state.ordernumber}
-                            className="border border-dark form-control" required />
-                    </div>
-                    <div className="inputsgroup my-2 w-25">
-                        <label htmlFor='shipingdate' className="fw-bold form-label">Shiping Date</label>
-                        <input type={'date'}
-                            onChange={this.handleShipingDataChange}
-                            value={this.state.shipingdata}
-                            className="border border-dark form-control" required />
-                    </div>
-                    <div className="inputsgroup my-2 w-25">
-                        <label htmlFor='destination' className="fw-bold form-label">Destination</label>
-                        <input type="text"
-                            onChange={this.handleDestinationChange}
-                            value={this.state.destination}
-                            className="border border-dark form-control" required />
-                    </div> <br />
-                    <div className='text-center'>
-                        <button type={'submit'} data-bs-toggle="modal"
-                            data-bs-target="#my-modal"
-                            className="mb-3 fw-bold btn btn-secondary">Submit</button>
-                    </div>
-                    <div id='my-modal' className="modal text-center" tabIndex={-1}>
-                        <div className="modal-dialog">
-                            <div className="modal-content">
-                                <div className="modal-header">
-                                    <h5 className="modal-title"> Are You Sure ? </h5>
-                                    <button type="button" className="btn-close"
-                                        data-bs-dismiss="modal" aria-label="Close"></button>
-                                </div>
-                                <div className="modal-footer">
-                                    <button type="button"
-                                        className="btn btn-secondary"
-                                        OnClientClick="this.disabled = true; this.value = 'در حال پردازش اطلاعات ...';"
-                                        UseSubmitBehavior="false"
-                                        onClick={this.handlesubmit}
-                                        data-bs-dismiss="modal">Yes</button>
-                                    <button type="button" className="btn btn-secondary"
-                                        data-bs-dismiss="modal" >No </button>
-                                </div>
+                <h4>Create Order</h4>
+                <div className="inputsgroup my-2 w-25">
+                    <label htmlFor='username' className="fw-bold form-label">User Name</label>
+                    <input type="text"
+                        value={this.state.username}
+                        onChange={this.handleUsernameChange}
+                        className="border border-dark form-control" required />
+                </div>
+                <div className="inputsgroup my-2 w-25">
+                    <label htmlFor='createdat' className="fw-bold form-label">Created at</label>
+                    <input type={'date'}
+                        onChange={this.handleCreatedAtChange}
+                        value={this.state.createdAt}
+                        className="border border-dark form-control" required />
+                </div>
+                <div className="inputsgroup my-2 w-25">
+                    <label htmlFor='source' className="fw-bold form-label">Source</label>
+                    <input type="text"
+                        onChange={this.handleSourceChange}
+                        value={this.state.source}
+                        className="border border-dark form-control" required />
+                </div>
+                <div className="inputsgroup my-2 w-25">
+                    <label htmlFor='warehouse' className="fw-bold form-label">Warehouse</label>
+                    <input type="text"
+                        onChange={this.handleWarehouseChange}
+                        value={this.state.Warehouse}
+                        className="border border-dark form-control" required />
+                </div>
+                <div className="inputsgroup my-2 w-25">
+                    <label htmlFor='ordernumer' className="fw-bold form-label">Order Number</label>
+                    <input type={'number'}
+                        onChange={this.handleOrderNumberChange}
+                        value={this.state.ordernumber}
+                        className="border border-dark form-control" required />
+                </div>
+                <div className="inputsgroup my-2 w-25">
+                    <label htmlFor='shipingdate' className="fw-bold form-label">Shiping Date</label>
+                    <input type={'date'}
+                        onChange={this.handleShipingDataChange}
+                        value={this.state.shipingdata}
+                        className="border border-dark form-control" required />
+                </div>
+                <div className="inputsgroup my-2 w-25">
+                    <label htmlFor='destination' className="fw-bold form-label">Destination</label>
+                    <input type="text"
+                        onChange={this.handleDestinationChange}
+                        value={this.state.destination}
+                        className="border border-dark form-control" required />
+                </div> <br />
+                <div className='text-center'>
+                    <button type={'submit'} data-bs-toggle="modal"
+                        data-bs-target="#my-modal"
+                        className="mb-3 fw-bold btn btn-secondary">Submit</button>
+                </div>
+                <div id='my-modal' className="modal text-center" tabIndex={-1}>
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title"> Are You Sure ? </h5>
+                                <button type="button" className="btn-close"
+                                    data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div className="modal-footer">
+                                <button type="button"
+                                    className="btn btn-secondary"
+                                    OnClientClick="this.disabled = true; this.value = 'در حال پردازش اطلاعات ...';"
+                                    UseSubmitBehavior="false"
+                                    onClick={this.handlesubmit}
+                                    data-bs-dismiss="modal">Yes</button>
+                                <button type="button" className="btn btn-secondary"
+                                    data-bs-dismiss="modal" >No </button>
                             </div>
                         </div>
                     </div>
-                </form>
+                </div>
             </div >
         )
     }
